6aeb8d363fe025217d850e46e7eb7e8c18274cdd
Van Kichline <vkichline@hotmail.com>
2020-08-20 10:27:38 -0700

    added define for # memories

--- lib/Calculator/TextCalculator.h
+++ lib/Calculator/TextCalculator.h
@@ -7,6 +7,7 @@ enum CalcMode { Calc_Mode_FP, Calc_Mode_Integer };
 enum CalcBase { Calc_Base_Binary = 2, Calc_Base_Octal = 8, CalcBase_Decimal = 10, Calc_Base_Hexidecimal = 16 };
 enum CalcTrigMode { Calc_Trig_Mode_Degrees, Calc_Trig_Mode_Radians, Calc_Trig_Mode_Grads };
 
+#define NUM_CALC_MEMORIES   100
 #define ERROR_UNKNOWN_MODE  -100
 
 // This class contains the MemoryCalculator and provides an implementation that uses
@@ -35,13 +36,13 @@ class TextCalculator {
     void      clear_memory();
     void      clear_all_memory();
   protected:
-    MemoryCalculator<double, 10>  _calc;
-    std::set<uint8_t>             _ops;
-    std::set<char>                _nums;
-    std::set<char>                _wspace;
-    CalcMode                      _mode;
-    uint8_t                       _precision;
-    Op_Err                        _error;
-    String                        _double_to_string(double val);
-    double                        _string_to_double(const char* val);
+    MemoryCalculator<double, NUM_CALC_MEMORIES> _calc;
+    std::set<uint8_t>                           _ops;
+    std::set<char>                              _nums;
+    std::set<char>                              _wspace;
+    CalcMode                                    _mode;
+    uint8_t                                     _precision;
+    Op_Err                                      _error;
+    String                                      _double_to_string(double val);
+    double                                      _string_to_double(const char* val);
 };
